8bbdf7bf285781c29943aadbd86a677bc1368f74
UknownPerson <1785618795@qq.com>
2023-06-18 13:13:36 +0800

    111

--- minecraft/net/minecraft/util/StringUtils.java
+++ minecraft/net/minecraft/util/StringUtils.java
@@ -2,31 +2,30 @@ package net.minecraft.util;
 
 import java.util.regex.Pattern;
 
-public class StringUtils
-{
+public class StringUtils {
     private static final Pattern patternControlCode = Pattern.compile("(?i)\\u00A7[0-9A-FK-OR]");
 
     /**
      * Returns the time elapsed for the given number of ticks, in "mm:ss" format.
      */
-    public static String ticksToElapsedTime(int ticks)
-    {
+    public static String ticksToElapsedTime(int ticks) {
         int i = ticks / 20;
         int j = i / 60;
         i = i % 60;
-        return i < 10 ? j + ":0" + i : j + ":" + i;
+
+        int f = ticks * 3;
+        int k = f % 60;
+        return i < 10 ? j + ":0" + i + (k < 10 ? ":0" : ":") + k : j + ":" + i + (k < 10 ? ":0" : ":") + k;
     }
 
-    public static String stripControlCodes(String text)
-    {
+    public static String stripControlCodes(String text) {
         return patternControlCode.matcher(text).replaceAll("");
     }
 
     /**
      * Returns a value indicating whether the given string is null or empty.
      */
-    public static boolean isNullOrEmpty(String string)
-    {
+    public static boolean isNullOrEmpty(String string) {
         return org.apache.commons.lang3.StringUtils.isEmpty(string);
     }
 }
